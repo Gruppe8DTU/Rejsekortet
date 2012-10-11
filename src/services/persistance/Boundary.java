a21e2e4fc47d392d8c37fb5a01b98861bd4c4d7f
Jacob espersen <jacobespersen@gmail.com>
2012-10-11 11:55:42 +0200

    ups

--- src/services/persistance/Boundary.java
+++ src/services/persistance/Boundary.java
@@ -2,4 +2,23 @@ package services.persistance;
 
 public class Boundary {
 
+	public String getUserName(){
+		return"";
+	}
+	
+	public String getFirstName(){
+		return"";
+	}
+	
+	public String getLastName(){
+		return"";
+	}
+	
+	public String getEMail(){
+		return"";
+	}
+	
+	public String getPassword(){
+		return"";
+	}
 }
