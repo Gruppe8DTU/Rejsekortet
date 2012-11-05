cc23714a3d81b22c67d46f1ee31c8aa0ac7a77e3
Jacob espersen <jacobespersen@gmail.com>
2012-11-05 15:18:54 +0100

    jacob is back

--- src/services/persistance/StartController.java
+++ src/services/persistance/StartController.java
@@ -26,7 +26,6 @@ public class StartController {
 		resetVar();
 	}
 	public void logIn(){
-		String uName
 	}
 	
 	/*
--- src/services/persistance/UserController.java
+++ src/services/persistance/UserController.java
@@ -3,28 +3,7 @@ package services.persistance;
 public class UserController {
 	Boundary bound;
 	
-<<<<<<< HEAD
-	public UserController(){
-		bound = new Boundary();
-=======
-	/*
-	 * aaaaaad
-	 * Opretter nye bruger i databasen
-	 * F¿rst kalder den getUserInfo som henter infoen om brugeren og assigner de n¿dvendige variabler.
-	 * Derefter kalder den createUser i SQL_Connect klassen som gemmer brugeren i databasen
-	 * sidst s¾tter den variablerne til empty String sŒ den er klar til n¾ste gang.
-	 */
-	public void createUser(){
-		getUserInfo();
-		new User(userName,firstName,lastName,eMail,password);
-		try {
-			connect.createUser(user);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		resetVar();
->>>>>>> test
-	}
+
 	
 	public void loggedInMenu(User user){
 		bound.displayLoggedInMenu();
